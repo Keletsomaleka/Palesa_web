1f9c958c34b8a9d939b9b17f3825693155d27347
keletsomaleka <keletsomaleka@yahoo.co.za>
2023-03-03 19:17:14 +0200

    change procfile content

--- backend/DietitianPalesa/settings.py
+++ backend/DietitianPalesa/settings.py
@@ -99,8 +99,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kmaleka47@gmail.com'
-EMAIL_HOST_PASSWORD = 'urqgjgjlmskbldhm'
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
 EMAIL_USE_TLS = True
 
 
